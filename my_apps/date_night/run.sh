e4de7d7a12fc6c28050d102a381c73268bb3526a
bhishanpdl <bhishantryphysics@gmail.com>
2025-01-22 10:11:20 -0500

    run.sh

---
+++ my_apps/date_night/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Bhishan (Bhishan@BpMacpro.local)
+# @file        : run
+# @created     : Wednesday Jan 22, 2025 10:07:33 EST
+#
+# @description : 
+######################################################################
+open http:/127.0.0.1:8000
+uvicorn app:app --reload
+
+
+
