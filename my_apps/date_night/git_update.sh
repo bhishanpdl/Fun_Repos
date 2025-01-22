821d0d2e1bf58b46316da1c50c317423ad32a979
bhishanpdl <bhishantryphysics@gmail.com>
2025-01-22 10:14:40 -0500

    fun night

---
+++ my_apps/date_night/git_update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Bhishan (Bhishan@BpMacpro.local)
+# @file        : git_update
+# @created     : Wednesday Jan 22, 2025 10:14:05 EST
+#
+# @description  
+######################################################################
+git add --all
+git commit -m "fun night"
+git push
+
+
--- my_apps/date_night/run.sh
+++ my_apps/date_night/run.sh
@@ -5,9 +5,29 @@
 # @file        : run
 # @created     : Wednesday Jan 22, 2025 10:07:33 EST
 #
-# @description : 
+# @description : Fun game
 ######################################################################
-open http:/127.0.0.1:8000
+#!/bin/bash
+
+# URL to open
+URL="http://127.0.0.1:8000"
+
+# Detect the operating system
+OS=$(uname)
+
+if [[ "$OS" == "Darwin" ]]; then
+    # macOS: Use the 'open' command
+    open "$URL"
+elif [[ "$OS" == "Linux" ]]; then
+    # Linux: Use 'xdg-open' (common for Linux systems)
+    xdg-open "$URL"
+elif [[ "$OS" =~ ^CYGWIN|MINGW|MSYS ]]; then
+    # Windows: Use 'start' command for Google Chrome
+    start chrome "$URL"
+else
+    echo "Unsupported operating system: $OS"
+fi
+
 uvicorn app:app --reload
 
 
